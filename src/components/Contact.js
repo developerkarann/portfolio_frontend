09346a4402478399abe6de30c15750bf16cd7554
Karan <karanpal03040@gmail.com>
2023-05-08 11:48:41 +0530

    fix server url issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,8 +30,8 @@ export default function Contact() {
     e.preventDefault();
 
     const { name, email, subject, description } = inpval;
-    const response = await fetch('http://localhost:5000/api/contact', {
-    // const response = await fetch('https://portfolio-6a26.onrender.com/api/contact', {
+    // const response = await fetch('http://localhost:5000/api/contact', {
+    const response = await fetch('https://portfolio-6a26.onrender.com/api/contact', {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
