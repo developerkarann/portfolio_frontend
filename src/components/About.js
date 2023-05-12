15de0f47a93f86f8ab3e5bc0cde0a6bbc2a10119
Karan <karanpal03040@gmail.com>
2023-05-12 18:26:29 +0530

    about in skills and about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,41 +5,46 @@ import profile from '../img/picture.jpg'
 export default function About() {
   return (
     <>
-    <section className="about" id="about">
-      <div className="max-width">
-        <h2 className="title">About Me</h2>
-        <div className="about-content">
-          <div className="column left">
-            <img src={profile}alt="" />
-          </div>
-          <div className="column right">
-            <div className="text">
-              I'm Karan Pal and I'm a <span className="typing-2"> 
-              <Typewriter
-                words={['JavaScript Developer', 'MERN Stack Developer', 'React Developer', 'Node Developer']}
-                loop={5}
-                cursor
-                cursorStyle='_'
-                typeSpeed={70}
-                deleteSpeed={50}
-                delaySpeed={1000}
-              />
-               </span>
+      <section className="about" id="about">
+        <div className="max-width">
+          <h2 className="title">About Me</h2>
+          <div className="about-content">
+            <div className="column left">
+              <img src={profile} alt="" />
             </div>
-            <p>
-              Hello! I am pursuing graduation with Commerce. I am excited to
-              begin my career as a full stack web developer and have been
-              actively learning and building projects on my own to gain
-              experience in front-end and back-end development. and am excited
-              to apply my passion for web development to real-world projects and
-              challenges. I am a quick learner and am committed to delivering
-              high-quality work.
-            </p>
-            <a href="/Resume.pdf" download="">Download Resume</a>
+            <div className="column right">
+              <div className="text">
+                I'm Karan Pal and I'm a <span className="typing-2">
+                  <Typewriter
+                    words={['JavaScript Developer', 'MERN Stack Developer', 'React Developer', 'Node Developer']}
+                    loop={5}
+                    cursor
+                    cursorStyle='_'
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                  />
+                </span>
+              </div>
+              <p>
+                Pursuing Bachelor Of Commerce from Hindu College Moradabad with a focus on web development
+                and have gained experience in various technologies such as
+                HTML, CSS, JavaScript, React.js, MongoDB, Express.js, and Node.js.. <br/>
+                I am excited to leverage my knowledge and skills to create robust and
+                responsive web applications using the MERN stack. I'm having a strong foundation in web development concepts and technologies,
+                and I am enthusiastic about learning new skills and staying up-to-date with the latest trends
+                in the industry. <br/>
+                As a fresher, I am committed to working collaboratively with other developers and designers
+                to deliver high-quality web applications that meet the unique needs of each client.
+                I am a quick learner, and I am always willing to go the extra mile to ensure that the
+                project is completed on time and to the highest standards.
+
+              </p>
+              <a href="/Resume.pdf" download="">Download Resume</a>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   )
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,20 +11,32 @@ export default function Skills() {
                         <div className="column left">
                             <div className="text">My Creative Skills & Expriences.</div>
                             <p>
-                                I have a strong foundation in HTML, CSS, Bootstrap and JavaScript
-                                , and have experience using server-side technologies such as PHP.
-                                I have also gained familiarity with DBMS(MySQL) and react.js
-                                through coursework and personal projects. I am eager to continue
-                                learning and expanding my skillset, I am confident that my skills
-                                and determination will make me a valuable asset to any team. <br />
+                                specialize in developing web applications using powerful technologies:
+                                MongoDB, Express.js, React.js, and Node.js. I have honed my skills in creating efficient, scalable, and user-friendly
+                                web applications that meet the unique needs of each client <br /> <br />
+
+                                My skills in React.js enable me to create highly interactive and responsive user interfaces that provide
+                                an engaging user experience. Finally, my expertise in Node.js allows me to develop fast and reliable
+                                server-side applications that can handle multiple concurrent requests.  <br /> <br />
+
+
+                                My skills in MongoDB allows me to develop dynamic databases that can handle large amounts
+                                of data . Additionally, I am good in using Express.js
+                                to build RESTful API and robust web servers that can handle complex API requests and integrate with various front-end
+                                frameworks. <br /> <br />
+
+                                Overall, I am passionate about Web Development and eager to contribute my skills and knowledge to create
+                                innovative and impactful web applications using the MERN stack. With a strong foundation in
+                                web development and a willingness to learn and grow, I am confident that I will be an asset to
+                                any team or project.
                             </p>
 
-                            <p className="certiInfo">
+                            {/* <p className="certiInfo">
                                 And i have completed Web Development Training course and got a
                                 certificate from internshala trainings where i learned both
                                 front-end and back-end web technologies .
-                            </p>
-                            <a rel="noreferrer" href="https://drive.google.com/file/d/1rKKISBzyMv7G6noNQNjHyu77llc-3oeW/view?usp=share_link" target="_blank">  Certificate <i className="fa-solid fa-award"></i></a>
+                            </p> */}
+                            {/* <a rel="noreferrer" href="https://drive.google.com/file/d/1rKKISBzyMv7G6noNQNjHyu77llc-3oeW/view?usp=share_link" target="_blank">  Certificate <i className="fa-solid fa-award"></i></a> */}
                         </div>
                         <div className="column right">
                             <div className="bars">
@@ -44,20 +56,20 @@ export default function Skills() {
                             <div className="bars">
                                 <div className="info">
                                     <span>JavaScript</span>
-                                    <span>65%</span>
+                                    <span>70%</span>
                                 </div>
                                 <div className="lines javascript"></div>
                             </div>
                             <div className="bars">
                                 <div className="info">
-                                    <span>PHP</span>
-                                    <span>70%</span>
+                                    <span>React.js</span>
+                                    <span>75%</span>
                                 </div>
                                 <div className="lines php"></div>
                             </div>
                             <div className="bars">
                                 <div className="info">
-                                    <span>mySQL</span>
+                                    <span>Node.js</span>
                                     <span>80%</span>
                                 </div>
                                 <div className="lines mysql"></div>
@@ -65,15 +77,29 @@ export default function Skills() {
 
                             <div className="bars">
                                 <div className="info">
-                                    <span>Bootstrap</span>
+                                    <span>Express.js</span>
                                     <span>80%</span>
                                 </div>
                                 <div className="lines Bootstrap"></div>
                             </div>
                             <div className="bars">
                                 <div className="info">
-                                    <span>Photoshop</span>
+                                    <span>MongoDB</span>
                                     <span>90%</span>
+                                </div>
+                                <div className="lines photoshop"></div>
+                            </div>
+                            <div className="bars">
+                                <div className="info">
+                                    <span>Github & Git</span>
+                                    <span>80%</span>
+                                </div>
+                                <div className="lines photoshop"></div>
+                            </div>
+                            <div className="bars">
+                                <div className="info">
+                                    <span>PHP & MySql</span>
+                                    <span>65%</span>
                                 </div>
                                 <div className="lines photoshop"></div>
                             </div>
