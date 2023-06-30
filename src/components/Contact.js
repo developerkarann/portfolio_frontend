0a0d584cd843d3e5eec08b8c56ff4304a06d7a59
Karan <karanpal03040@gmail.com>
2023-06-30 20:45:05 +0530

    fix server url

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,7 +37,7 @@ export default function Contact() {
     const { name, email, subject, description } = inpval;
 
     // const response = await fetch('http://localhost:5000/api/contact', {
-      const response = await fetch('https://portfolio-backend-ten-black.vercel.app/', {
+      const response = await fetch('https://portfolio-backend-ten-black.vercel.app/api/contact', {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -54,14 +54,13 @@ export default function Contact() {
       toast.warning("Please fill all the fields", {
         position: "top-center",
         autoClose: 5000,
-        isLoading: false,
+   
       })
     } else {
       setInp({ ...inpval, name: "", email: "", subject: "", description: "" })
-      toast.success("Message send successfully ❤", {
+      toast.success("Thank You ❤ ", {
         position: "top-center",
         autoClose: 5000,
-        isLoading: false,
       })
     }
   }
