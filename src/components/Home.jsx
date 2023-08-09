7527440f8d9fa1522aec89e6a87717a9ae75092e
Karan Pal <karanpal03040@gmail.com>
2023-08-09 06:07:21 -0700

    change home page image

---
+++ src/components/Home.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typewriter } from 'react-simple-typewriter'
+
+import Service from './Service';
+import Skills from './Skills';
+import About from './About'
+import Contact from './Contact'
+import Projects from './Projects';
+import leftImg from '../img/krn.jpg'
+
+export default function Home() {
+
+
+  return (
+    <>
+      <section className="home" id="home">
+        <div className="max-width">
+          <div className="home_section">
+            <div className="home-content">
+              <div className="text-1">Hello , my self</div>
+              <div className="text-2">Karan Pal</div>
+              <div className="text-3">And I'm a <Typewriter
+                words={['JavaScript Developer', 'MERN Stack Developer', 'React Developer', 'Node Developer']}
+                loop={5}
+                cursor
+                cursorStyle='_'
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+              /> <span className="typing"> </span>
+              </div>
+              <Link to="/contact">Hire me</Link>
+            </div>
+            <div className="right_side">
+              <div className="box">
+                <div className="border"></div>
+                <img src={leftImg} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <About />
+      <Service />
+      <Skills />
+      <Projects />
+      <Contact />
+
+    </>
+  )
+}
