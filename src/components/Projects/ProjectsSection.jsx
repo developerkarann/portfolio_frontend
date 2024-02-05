093ac343c8af4647a3c6ac48b911e8a0b4149e1f
Karan Pal <karanpal03040@gmail.com>
2024-02-05 20:22:30 +0530

    fix issues in projects section

--- src/components/Projects/ProjectsSection.jsx
+++ src/components/Projects/ProjectsSection.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
 import './style.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 export default function ProjectsSection() {
     const ProjectsData = [
         {
             title: 'Gyaanta - Ed-Tech App',
-            tech: 'React.js, Node.js, Express.js, MongoDb',
+            tech: 'MERN Stack Project',
             description: "Gyaanta! is a real world project. It's an education website that offers a wide range of courses and interactive learning experiences. Whether you're a student or just curious to learn new things, Gyaanta has you covered.",
             img: '/assets/images/projects/gyaanta.png',
-            link: 'http://gyaanta.com',
+            link: 'https://gyaanta.netlify.app/',
             github: 'https://github.com/jainsarwang/ProjectBright',
+        },
+        {
+            title: 'Vedic Store',
+            tech: 'MERN Stack Project',
+            description: "Vedic Store is an Ecommerce store which sells Authentic Hindu Scriptures and spiritual Products",
+            img: '/assets/images/projects/vedicstore.png',
+            link: 'https://vedicstore.vercel.app',
+            github: 'https://github.com/developerkarann/Ultimate-Ecommerce',
         },
         {
             title: 'MySpotify - Music App',
             tech: ' HTML, CSS,  JavaScript',
             description: "MySpotify is a music streaming website which provide sevaral playlist accrouding to the mood by them user can listen songs without dowanload them and without any intruption. This is project is build in pure HTMl, CSS and JavaScript",
             img: '/assets/images/projects/spotify.png',
-            link: 'https://karanpal03040.github.io/myspotify',
-            github: 'https://github.com/karanpal03040/myspotify',
-        },
-        {
-            title: 'FavFood',
-            tech: 'React.js, Node.js, Express.js, MongoDb, Bootstrap ',
-            description: "FavFood is a E-commerce food devluvering website where diffirent varity of fast food. You have to login or creat account to order any food",
-            img: '/assets/images/projects/FavFood.png',
-            link: 'https://favfoodie.netlify.app',
-            github: 'https://github.com/karanpal03040/favfoodf_frontend',
+            link: 'https://developerkarann.github.io/myspotify',
+            github: 'https://github.com/developerkarann/myspotify',
         },
         {
             title: 'Magic Notes',
@@ -35,17 +35,10 @@ export default function ProjectsSection() {
             description: '/assets/images/projects/notes.png',
             description: "Gyaanta! It's an education website that offers a wide range of courses and interactive learning experiences. Whether you're a student or just curious to learn new things, Gyaanta has you covered.",
             img: '/assets/images/projects/notes.png',
-            link: 'https://karanpal03040.github.io/notes',
-            github: 'https://github.com/karanpal03040/notes',
+            link: 'https://developerkarann.github.io/notes',
+            github: 'https://github.com/developerkarann/notes',
         },
-        {
-            title: 'CRUD App',
-            tech: 'React.js, Node.js, Express.js, MongoDb ',
-            description: "Gyaanta! It's an education website that offers a wide range of courses and interactive learning experiences. Whether you're a student or just curious to learn new things, Gyaanta has you covered.",
-            img: '/assets/images/projects/CRUD.png',
-            link: 'https://crudio.netlify.app',
-            github: 'https://github.com/karanpal03040/crud_frontend',
-        },
+
     ]
     return (
         <>
@@ -59,6 +52,7 @@ export default function ProjectsSection() {
 
                             ProjectsData.map((value, index) => {
                                 return (
+
                                     <div id="card">
                                         <div class="slide slide1">
                                             <div class="content">
@@ -72,14 +66,15 @@ export default function ProjectsSection() {
                                         <div class="slide slide2">
                                             <div class="content">
                                                 <h2>
-                                                   {value.title}
+                                                    {value.title}
                                                 </h2>
                                                 <p className='desc'>{value.description}</p>
                                                 <p className='technology'>{value.tech}</p>
-                                               <p><Link className='live_link'  to={value.link}  target='_blank'>{value.link}</Link></p> 
+                                                <p><Link className='live_link' to={value.link} target='_blank'>{value.link}</Link></p>
                                             </div>
                                         </div>
                                     </div>
+
                                 )
                             })
                         }
